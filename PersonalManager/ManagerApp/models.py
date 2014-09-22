363cfbb5e1a23111229dac0397ed26b85a8d2ecc
nuclearBeer <smartbbapp@yandex.ru>
2014-09-22 19:02:32 +0300

    Update models.py

--- PersonalManager/ManagerApp/models.py
+++ PersonalManager/ManagerApp/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from yaml import *
 from ModelsBuilder import *
-yaml_file_path = '/home/vprokopenko/PycharmProjects/new_personal/PersonalManager/ManagerApp/model.yaml'
+yaml_file_path = 'PersonalManager/ManagerApp/model.yaml'
 
 
 classes_list = build_class_from_yaml(yaml_file_path)
